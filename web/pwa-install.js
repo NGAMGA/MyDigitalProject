e211be0b15cbd97535433bbd692274dfc7cc787e
BERHAULT Nolan <nolan.berhault@users.noreply.github.com>
2026-06-22 15:42:42 +0200

    Add a persistent Komi PWA service worker

--- web/pwa-install.js
+++ web/pwa-install.js
@@ -1,7 +1,7 @@
 (function () {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      navigator.serviceWorker.register('flutter_service_worker.js');
+      navigator.serviceWorker.register('komi_service_worker.js');
     });
   }
 
